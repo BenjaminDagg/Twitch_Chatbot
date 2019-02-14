f1c4799400e255ee803640366f859c569a700c81
BenjaminDagg <bdagg@csumb.edu>
2019-02-14 01:37:56 -0800

    initial commit. Basic Twitch API call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+
+const clientID = 'vcot0kpqo1jbrdodynosldxmimawiy';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+
+    var url = 'https://api.twitch.tv/kraken/search/channels?query=starcraft';
+    var config = {
+      headers: {
+        'Client-ID':clientID
+      }
+    };
+    axios.get(url,config)
+        .then(res => {
+          console.log(res);
+        })
+
+  }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
